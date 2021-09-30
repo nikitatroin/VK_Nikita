bae42606faf3d1acea9cc29b0df9740c27ded302
[nikitatroin] <[nikitatroin@yandex.ru]>
2021-09-30 18:10:38 +0300

    added News

--- VK_Nikita/Cells/News/NewsCell.swift
+++ VK_Nikita/Cells/News/NewsCell.swift
@@ -23,20 +23,17 @@ class NewsCell: UICollectionViewCell {
         self.addSubview(nameLabel)
         self.addSubview(profileImageView)
         
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0(44)]-8-[v1]", options:NSLayoutConstraint.FormatOptions(rawValue: 0) , metrics: nil,
-                                                      views: ["v0":profileImageView,
-                                                              "v1":nameLabel,]))
+        addConstWithFormat(format: "H:|-8-[v0(44)]-8-[v1]", views: profileImageView, nameLabel)
+        addConstWithFormat(format: "V:|-8-[v0]", views: nameLabel)
+        addConstWithFormat(format: "V:|-8-[v0(44)]", views: profileImageView)
         
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0]", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: ["v0":nameLabel]))
-        
-        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0(44)]", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: ["v0":profileImageView]))
-        
+
     }
     
     let profileImageView:UIImageView = {
         let imageView = UIImageView()
+        imageView.image = UIImage(named: "Я")
         imageView.contentMode = .scaleAspectFit
-        imageView.backgroundColor = UIColor.red
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
@@ -44,16 +41,39 @@ class NewsCell: UICollectionViewCell {
     
     let nameLabel: UILabel = {
         let lable = UILabel()
-        lable.text = "Sample Name"
-        lable.font = UIFont.boldSystemFont(ofSize: 20)
-        lable.translatesAutoresizingMaskIntoConstraints = false
+        lable.numberOfLines = 2
+        
+        let attributedText = NSMutableAttributedString(string: "Nikita Troyan",attributes: [NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 15)])
+        
+        attributedText.append(NSAttributedString(string: "\n· September 28 · Saint Petersburg ·", attributes:
+                                                    [NSAttributedString.Key.font:UIFont.boldSystemFont(ofSize: 13),
+                                                    NSAttributedString.Key.foregroundColor: UIColor(red: 155/255, green: 161/255, blue: 171/255, alpha: 1)]))
+        
+        lable.attributedText = attributedText
+        
         return lable
     }()
     
 }
 
 extension UIView {
-    func addConstWithFormat(format: String, view: UIView...) {
-        
+    // создаём расширение для UIView, добавляем функцию, в ней первый элемент это VisualFormat, второе это массив всех View
+    func addConstWithFormat(format: String, views: UIView...) {
+        //далее создаётся словарь, где VisualFormat приклеплён к определённой View
+        var viewDict = [String:UIView]()
+        //далее берём массив всех полученных View, перебераем View и её index
+        for (index, view) in views.enumerated() {
+            //индекс используем как число после v
+            let key = "v\(index)"
+            //а под этим key, будет хранится одна из view нашего массива из общих view
+            viewDict[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+            //когда данное свойство у view включено мы можем модифицировать только frame, bounds and center свойство, а xcode в зависимости от этого задаёт расположение для view.
+            //когда мы из кода задаём constraints, мы ограничиваем view в пространстве superView и она занимает размер в соответствии с этими правилами
+            //
+        }
+        // добавили constaint, в который будет передаваться наш VisualFormat и наш словарь, которых хранит VisualFormat и view
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutConstraint.FormatOptions.init(rawValue: 0), metrics: nil, views: viewDict))
     }
 }
+//то есть есть один массив из всех view на superView, есть словарь, которых хранит VisualFormat типа "v0" или "v1", как ключ и значение, то есть саму view, которую требуется установить
